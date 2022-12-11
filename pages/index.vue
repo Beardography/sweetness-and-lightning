9299755a3f24c2918848b38df5f0e050000e3077
Beardography <ryan.l.huber@gmail.com>
2022-12-11 17:15:35 -0600

    setting up styles and a Card component

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,25 @@
         <Nav />
         <main class="home">
             <h1>Tester</h1>
+            <div class="card-group">
+                <ContentSlot>
+                    <Card
+                        title="tester"
+                        servingSize="4"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mauris pharetra et ultrices neque."
+                    />
+                    <Card
+                        title="recipe name"
+                        servingSize="3"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mauris pharetra et ultrices neque."
+                    />
+                    <Card
+                        title="soup"
+                        servingSize="12"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mauris pharetra et ultrices neque."
+                    />
+                </ContentSlot>
+            </div>
         </main>
     </div>
 </template>
