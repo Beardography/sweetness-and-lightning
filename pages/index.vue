4c30bffdae3c6df7a9e888890b8a7df8ea224cb9
Beardography <ryan.l.huber@gmail.com>
2022-12-11 18:54:00 -0600

    fixed the NuxtLink link to the recipe

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,14 @@
             <h1>Tester</h1>
             <div class="card-group">
                 <ContentSlot>
-                    <NuxtLink to="/recipes/donabe_rice.md">
+                    <NuxtLink to="/recipes/donabe_rice">
                         <Card
                             title="tester"
                             servingSize="4"
                             description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mauris pharetra et ultrices neque."
                         />
                     </NuxtLink>
-                    <Card
+                    <!-- <Card
                         title="recipe name"
                         servingSize="3"
                         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mauris pharetra et ultrices neque."
@@ -21,7 +21,7 @@
                         title="soup"
                         servingSize="12"
                         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget mauris pharetra et ultrices neque."
-                    />
+                    /> -->
                 </ContentSlot>
             </div>
         </main>
